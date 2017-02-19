bf0f5295c785b5d69c65817049f20f0515d8e80f
simoaweb <simoaweb@gmail.com>
2017-02-19 16:03:16 -0300

    compatibilidade mobile

--- js/init.js
+++ js/init.js
@@ -3,6 +3,16 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $(".dropdown-button").dropdown();
+    $(document).ready(function(){
+    $('.slider').slider({full_width: true});
+    });
+    
+      $(document).ready(function(){
+        $('.collapsible').collapsible({
+          accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+        });
+      });
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
